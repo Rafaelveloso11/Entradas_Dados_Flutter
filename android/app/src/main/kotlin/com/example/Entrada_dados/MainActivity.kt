c00b0ee144777f21b3e7c35d2c8b30f2ed50c326
Rafael Veloso <rafaelveloso11@gmail.com>
2021-02-21 12:32:45 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/Entrada_dados/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Entrada_dados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:Entrada_dados/EntradaRadionButton.dart';
+import 'package:Entrada_dados/EntradaSlider.dart';
+import 'package:Entrada_dados/EntradaSwitch.dart';
+
+import 'EntradaSlider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    //home: CampoTexto(),
+    home: EntradaSlider(),
+  ));
+}
